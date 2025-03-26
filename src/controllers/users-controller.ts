9eb72e3195ebfefd5b08a0f338b2889f5c90691f
AndreTrevizam <andretrevizam264@gmail.com>
2025-03-26 16:23:01 -0300

    add Prisma client setup, update package dependencies, and implement user creation with input validation and password hashing

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,22 @@
 import { Request, Response, NextFunction } from "express"
+import { z } from "zod"
+import { hash } from "bcrypt"
 
 class UsersController {
-    async create(req: Request, res: Response, next: NextFunction) {
-        return res.json("User created")
+    async create(req: Request, res: Response) {
+        // Validando os dados de entrada
+        const bodySchema = z.object({
+            name: z.string().trim().min(3),
+            email: z.string().email(),
+            password: z.string().min(6),
+        })
+
+        const { name, email, password } = bodySchema.parse(req.body)
+
+        // Criptografando a senha
+        const hashedPassword = await hash(password, 8)
+
+        return res.json(hashedPassword)
     }
 }
 
