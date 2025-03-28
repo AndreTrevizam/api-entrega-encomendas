ce8e26bfa7d55518cbbe9bf22ff0dc5553918cf6
AndreTrevizam <andretrevizam264@gmail.com>
2025-03-27 21:42:51 -0300

    add deliveries status controller and update route for status modification with validation

---
+++ src/controllers/deliveries-status-controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { z } from "zod"
+
+class DeliveriesStatusController {
+  async update(req: Request, res: Response) {
+    // Fazendo a validação do params (id) da requisição
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    // Fazendo a validação do body da requisição
+    // O status pode ser "processing", "shipped" ou "delivered"
+    const bodySchema = z.object({
+      status: z.enum(["processing", "shipped", "delivered"])
+    })
+
+    const { id } = paramsSchema.parse(req.params)
+    const { status } = bodySchema.parse(req.body)
+
+    // Faz a atualização do status da entrega
+    await prisma.delivery.update({
+      data: {
+        status
+      },
+      where: {
+        id
+      }
+    })
+
+    return res.json()
+  }
+}
+
+export { DeliveriesStatusController}
